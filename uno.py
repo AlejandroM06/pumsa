92013e17811ec19ae5ad0832a702fa4d091f0835
AlejandroM06 <amontesdom@gmail.com>
2026-02-12 22:01:35 -0600

    modified: notas para compilar windows.txt

--- uno.py
+++ uno.py
@@ -1,2 +1,17 @@
-#aprendete las palabras reservadas de python we
-print("Hello, World!")
+import os
+
+os.system("cls")
+a = input("Dame el valor de a: ")
+b = input("Dame el valor de b: ")
+
+
+if a == b:
+    print("a es igual a b")
+else:
+    print("nowe, a no es igual a b")
+    
+os.system("pause")
+os.system("cls")
+nombre = input("Dame tu nombre: ")
+os.system("cls")
+print("Hola " + nombre)
